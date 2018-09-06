4eff07880b9572331276c98de33a3d2548e930ff
Yang Feng <yang.feng@kaust.edu.sa>
2018-09-06 23:39:39 +0800

    add log-back

--- src/main/java/com/moxi/controller/AppController.java
+++ src/main/java/com/moxi/controller/AppController.java
@@ -6,9 +6,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
+
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +24,9 @@ import com.moxi.util.Constant;
 
 @RestController
 public class AppController {
-	
+
+	Logger logger = LoggerFactory.getLogger(AppController.class);
+
 	/*
 	 * GET请求
 	 */
--- src/main/java/com/moxi/recontroller/TestController.java
+++ src/main/java/com/moxi/recontroller/TestController.java
@@ -3,8 +3,10 @@ package com.moxi.recontroller;
 import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moxi.pojo.ScoreUpload;
+import com.moxi.pojo.Tester;
 import com.moxi.service.IKnowledgeService;
-import com.moxi.util.ExcelImportUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
+    Logger logger = LoggerFactory.getLogger(Tester.class);
+
     @Autowired
     IKnowledgeService iKnowledgeService;
 
@@ -26,7 +30,9 @@ public class TestController {
     @JsonRawValue
     public String testTheInterface(String seriesNumber) {
 
+
         ObjectMapper objectMapper = new ObjectMapper();
+
         ScoreUpload scoreUpload = iKnowledgeService.getScoreInfo(seriesNumber);
         if(scoreUpload == null) {
             return "it is empty";
@@ -36,6 +42,7 @@ public class TestController {
             return scoreToJson;
         }catch (Exception e) {
             System.out.println("error" + e.getMessage());
+            logger.error(e.getMessage(), e);
             return e.getMessage();
         }
 
